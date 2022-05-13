3f26e4553188116b714d2968526f60f14b3eaf2a
Joana Mateus <joanamateusg@gmail.com>
2022-05-13 11:50:24 +0100

    Initial structure of MainActivity, LoginActivity and RegisterActivity.

--- monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/LoginActivity.java
+++ monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/LoginActivity.java
@@ -4,25 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    // send to Login Activity
-    public void onLoginActivity(View v){
-        Intent loginActivity = new Intent(this, LoginActivity.class);
-        startActivity(loginActivity);
+        setContentView(R.layout.activity_login);
     }
 
     // send to Register Activity
-    public void onRegisterActivity(View v){
+    public void onResgiterActivity(View v){
         Intent registerActivity = new Intent(this, RegisterActivity.class);
         startActivity(registerActivity);
     }
--- monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/RegisterActivity.java
+++ monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/RegisterActivity.java
@@ -4,26 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
     // send to Login Activity
     public void onLoginActivity(View v){
         Intent loginActivity = new Intent(this, LoginActivity.class);
         startActivity(loginActivity);
-    }
-
-    // send to Register Activity
-    public void onRegisterActivity(View v){
-        Intent registerActivity = new Intent(this, RegisterActivity.class);
-        startActivity(registerActivity);
     }
 }
