739b9b88263e7aa6f2544acd0f44931ccf0ebf30
Thelusion <rafaelsouzacotrim@gmail.com>
2022-05-21 10:06:36 +0100

    save

--- monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/api/APIClient.java
+++ monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/api/APIClient.java
@@ -15,7 +15,7 @@ public class APIClient {
         }
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.0.15:8080")
+                .baseUrl("http://192.168.0.18:8080")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
