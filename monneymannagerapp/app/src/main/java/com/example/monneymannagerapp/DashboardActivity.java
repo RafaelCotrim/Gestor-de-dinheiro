b30e6b868524b888c385cb62f11e104ebf7b79c0
Joana Mateus <joanamateusg@gmail.com>
2022-06-01 22:07:28 +0100

    Main dashboard.

--- monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/DashboardActivity.java
+++ monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/DashboardActivity.java
@@ -3,7 +3,6 @@ package com.example.monneymannagerapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ActionBarContainer;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -14,15 +13,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.example.monneymannagerapp.Fragments.BudgetFragment;
 import com.example.monneymannagerapp.Fragments.MainFragment;
-import com.example.monneymannagerapp.Fragments.ProfileFragment;
-import com.example.monneymannagerapp.Fragments.StatisticFragment;
-import com.example.monneymannagerapp.api.dtos.UserDto;
 import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
--- monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/Fragments/MainFragment.java
+++ monneymannagerapp/app/src/main/java/com/example/monneymannagerapp/Fragments/MainFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +14,29 @@ import com.example.monneymannagerapp.R;
 //Dashboard
 public class MainFragment extends Fragment {
 
+    private TextView dayAmount, weekAmount, monthAmount, yearAmount;
+    private TextView username;
+    private TextView totalAmount;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.main_fragment, container, false);
+
+        dayAmount = view.findViewById(R.id.amount_spent_in_day);
+        weekAmount = view.findViewById(R.id.amount_spent_in_week);
+        monthAmount = view.findViewById(R.id.amount_spent_in_month);
+        yearAmount = view.findViewById(R.id.amount_spent_in_year);
+        username = view.findViewById(R.id.username);
+        totalAmount = view.findViewById(R.id.total_amount_view);
+
+        //TODO - procurar os dados na API
+        dayAmount.setText("30");    //TODO dado fake - remover
+        weekAmount.setText("130");  //TODO dado fake - remover
+        monthAmount.setText("400"); //TODO dado fake - remover
+        yearAmount.setText("5000"); //TODO dado fake - remover
+        username.setText("Mariana");//TODO dado fake - remover
+        totalAmount.setText("1300");//TODO dado fake - remover
+
         return view;
     }
 }
